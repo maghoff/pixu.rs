2687c3e14eb88751040b6a6fef137d1202be302b
Magnus Hovland Hoff <maghoff@gmail.com>
2018-11-07 08:56:09 +0100

    HTTP sever fundament

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 #[macro_use] extern crate diesel_migrations;
 extern crate diesel;
+extern crate hyper;
 extern crate r2d2_diesel;
 extern crate r2d2;
 extern crate structopt;
+extern crate tokio;
 
 mod db;
+
+use std::net::SocketAddr;
+use hyper::rt::Future;
 
 use structopt::StructOpt;
 
@@ -19,6 +24,26 @@ struct Options {
 fn main() -> Result<(), Box<std::error::Error>>{
     let opt = Options::from_args();
     let _db = db::create_pool(opt.db)?;
+
+    let bind_host = "127.0.0.1".parse().expect("Acceptable IP address");
+    let bind_port = 1212;
+
+    let service_fn = || {
+        use hyper::{Body, Response, service::service_fn_ok};
+        service_fn_ok(|_req| {
+            Response::new(Body::from("Hello World"))
+        })
+    };
+
+    let server =
+        hyper::server::Server::bind(&SocketAddr::new(bind_host, bind_port))
+            .serve(service_fn);
+
+    println!("Listening on http://{}", server.local_addr());
+
+    tokio::run(server.map_err(|e| {
+        eprintln!("server error: {}", e);
+    }));
 
     Ok(())
 }
