4f78c078a1a5172546f342b1a4e758fcd3d74d19
Magnus Hovland Hoff <maghoff@gmail.com>
2019-06-10 21:26:45 +0200

    Expose list of all available pixurs for logged in user

--- src/site/index.rs
+++ src/site/index.rs
@@ -1,12 +1,19 @@
+use diesel;
+use diesel::prelude::*;
+use diesel::sqlite::SqliteConnection;
 use futures::{compat::Stream01CompatExt, FutureExt, TryStreamExt};
 use hyper::http;
+use r2d2::Pool;
+use r2d2_diesel::ConnectionManager;
 use serde_urlencoded;
 use web::{Cookie, Error, FutureBox, MediaType, RepresentationBox, Resource, Response};
 
 use super::auth;
 use super::handling_error::HandlingError;
+use crate::db::schema::*;
 
 pub struct Index {
+    db_pool: Pool<ConnectionManager<SqliteConnection>>,
     claims: Option<auth::Claims>,
 }
 
@@ -14,6 +21,7 @@ pub struct Index {
 #[template = "templates/index.html"]
 struct Get<'a> {
     claims: &'a Option<auth::Claims>,
+    authorized_pixurs: &'a [i32],
 }
 
 #[derive(serde_derive::Deserialize)]
@@ -78,36 +86,56 @@ impl Index {
         })
     }
 
-    async fn post_core(
-        self: Box<Self>,
-        content_type: String,
-        body: hyper::Body,
-    ) -> Response {
+    async fn post_core(self: Box<Self>, content_type: String, body: hyper::Body) -> Response {
         self.try_post(content_type, body)
             .await
             .unwrap_or_else(|e| e.render())
+    }
+
+    async fn try_get(self: Box<Self>) -> Result<Response, HandlingError> {
+        let db_connection = self
+            .db_pool
+            .get()
+            .map_err(|_| HandlingError::InternalServerError)?;
+
+        let authorized_pixurs = self
+            .claims
+            .as_ref()
+            .map(|claims| {
+                pixur_authorizations::table
+                    .filter(pixur_authorizations::sub.eq(&claims.sub))
+                    .select(pixur_authorizations::pixur_id)
+                    .load::<i32>(&*db_connection)
+            })
+            .transpose()
+            .map_err(|_| HandlingError::InternalServerError)?
+            .unwrap_or_else(|| vec![]);
+
+        Ok(Response::new(
+            http::StatusCode::OK,
+            vec![(
+                MediaType::new("text", "html", vec!["charset=utf-8".to_string()]),
+                Box::new(move || {
+                    Box::new(
+                        Get {
+                            claims: &self.claims,
+                            authorized_pixurs: &authorized_pixurs,
+                        }
+                        .to_string(),
+                    ) as RepresentationBox
+                }) as _,
+            )],
+        ))
+    }
+
+    async fn get_core(self: Box<Self>) -> Response {
+        self.try_get().await.unwrap_or_else(|e| e.render())
     }
 }
 
 impl Resource for Index {
     fn get<'a>(self: Box<Self>) -> FutureBox<'a, Response> {
-        async {
-            Response::new(
-                http::StatusCode::OK,
-                vec![(
-                    MediaType::new("text", "html", vec!["charset=utf-8".to_string()]),
-                    Box::new(move || {
-                        Box::new(
-                            Get {
-                                claims: &self.claims,
-                            }
-                            .to_string(),
-                        ) as RepresentationBox
-                    }) as _,
-                )],
-            )
-        }
-            .boxed()
+        self.get_core().boxed()
     }
 
     fn post<'a>(
@@ -119,7 +147,9 @@ impl Resource for Index {
     }
 }
 
-pub struct IndexLoader;
+pub struct IndexLoader {
+    pub db_pool: Pool<ConnectionManager<SqliteConnection>>,
+}
 
 impl auth::ClaimsConsumer for IndexLoader {
     type Claims = auth::Claims;
@@ -128,6 +158,12 @@ impl auth::ClaimsConsumer for IndexLoader {
         self,
         claims: Option<Self::Claims>,
     ) -> FutureBox<'a, Result<Box<dyn Resource + Send + 'static>, Error>> {
-        async { Ok(Box::new(Index { claims }) as Box<dyn Resource + Send + 'static>) }.boxed() as _
+        async {
+            Ok(Box::new(Index {
+                claims,
+                db_pool: self.db_pool,
+            }) as Box<dyn Resource + Send + 'static>)
+        }
+            .boxed() as _
     }
 }
