5aca8ab543e03a4d60fa851c1a070277961da712
Magnus Hovland Hoff <maghoff@gmail.com>
2019-06-17 17:15:57 +0200

    Start integrating email sending into login flow prototype

--- src/bin/login-flow.rs
+++ src/bin/login-flow.rs
@@ -3,7 +3,14 @@ extern crate serde_derive;
 
 use chrono::{DateTime, Duration, Utc};
 use jsonwebtoken::{Algorithm, Header, Validation};
+use std::path::PathBuf;
 use structopt::StructOpt;
+
+use lettre::smtp::authentication::{Credentials, Mechanism};
+use lettre::smtp::ConnectionReuseParameters;
+use lettre::{ClientSecurity, SmtpClient, SmtpTransport, Transport};
+
+use lettre_email::{EmailBuilder, Mailbox};
 
 const KEY: &[u8] = b"secret";
 
@@ -30,13 +37,66 @@ struct Claims {
 }
 
 #[derive(StructOpt, Debug)]
-#[structopt(name = "login-flow")]
-enum Options {
+enum Operation {
     Issue { email: String },
     Verify { head_sign: String, claims: String },
 }
 
-fn issue(email: String) {
+#[derive(StructOpt, Debug)]
+#[structopt(name = "login-flow")]
+struct Options {
+    #[structopt(parse(from_os_str))]
+    config: PathBuf,
+
+    #[structopt(subcommand)]
+    operation: Operation,
+}
+
+#[derive(Debug, Deserialize)]
+struct EmailConfig {
+    host: String,
+    port: u16,
+
+    user: String,
+    password: String,
+
+    sender_name: String,
+    sender_email: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct Config {
+    email: EmailConfig,
+}
+
+fn is_registered_user(_email: &str) -> bool {
+    // TODO implement stub
+
+    true
+}
+
+fn maybe_send_email(email: &str, claims: &str, mailer: &mut SmtpTransport, sender: Mailbox) {
+    if !is_registered_user(email) {
+        return;
+    }
+
+    let base_url = "http://localhost/"; // FIXME
+
+    let redir = "1"; // FIXME
+    let verification_link = format!("{}/auth?validation={}&redir={}", base_url, claims, redir);
+
+    let email = EmailBuilder::new()
+        .to(email)
+        .from(sender)
+        .subject("Innlogging")
+        .text(format!("Følg denne linken: {}", verification_link))
+        .build()
+        .unwrap();
+
+    mailer.send(email.into()).unwrap();
+}
+
+fn issue(email: String, mailer: &mut SmtpTransport, sender: Mailbox) {
     let claims = Claims {
         phase: AuthPhase::Validation,
         sub: email.clone(),
@@ -51,8 +111,9 @@ fn issue(email: String) {
     let claims = parts.next().unwrap();
     let sign = parts.next().unwrap();
 
-    println!("head_sign: {}.{}", head, sign);
-    println!("claims: {}", claims);
+    maybe_send_email(&email, claims, mailer, sender);
+
+    println!("Set-Cookie: let-me-in={}.{}", head, sign);
 }
 
 fn verify_core(head_sign: &str, claims: &str) -> Result<Claims, Box<dyn std::error::Error>> {
@@ -87,9 +148,23 @@ fn verify(head_sign: &str, claims: &str) -> Result<(), Box<dyn std::error::Error
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let opt = Options::from_args();
 
-    match opt {
-        Options::Issue { email } => issue(email),
-        Options::Verify { head_sign, claims } => verify(&head_sign, &claims)?,
+    let config = std::fs::read_to_string(opt.config)?;
+    let config: Config = toml::from_str(&config)?;
+    let sender: Mailbox = (config.email.sender_email, config.email.sender_name).into();
+
+    let mut mailer = SmtpClient::new(
+        (config.email.host.as_str(), config.email.port),
+        ClientSecurity::None,
+    )?
+    .credentials(Credentials::new(config.email.user, config.email.password))
+    .smtp_utf8(true)
+    .authentication_mechanism(Mechanism::Plain)
+    .connection_reuse(ConnectionReuseParameters::ReuseUnlimited)
+    .transport();
+
+    match opt.operation {
+        Operation::Issue { email } => issue(email, &mut mailer, sender),
+        Operation::Verify { head_sign, claims } => verify(&head_sign, &claims)?,
     };
 
     Ok(())
