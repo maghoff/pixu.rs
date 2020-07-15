919bc9be1c79062d7a361e244681b4000322bde2
Magnus Hovland Hoff <maghoff@gmail.com>
2020-07-15 08:52:12 +0200

    Cleanup

--- src/bin/id30.rs
+++ src/bin/id30.rs
@@ -11,7 +11,6 @@ use id30::Id30;
 fn main() -> Result<(), &'static str> {
     let arg = env::args().skip(1).next();
 
-    // let id: Id30 = arg.map(|x| x.parse()).or_else(|| Ok(Id30::new(0)))?;
     let id = arg.map(|x| x.parse().map_err(|_| "Parse error"));
     let id = id.unwrap_or_else(|| {
         let mut rng = SmallRng::from_entropy();
